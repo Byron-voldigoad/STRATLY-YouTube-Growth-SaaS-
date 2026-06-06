fcee43caba95da4697f3b9f5b20be1260a3971e6
Byron-voldigoad <tchangolouismullergmail.com>
2026-06-06 13:25:56 +0100

    Configure CORS and production environment variables 3

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: "'https://logfwxffbzpqrcgzrkwl.supabase.co",
+  supabaseUrl: "https://logfwxffbzpqrcgzrkwl.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvZ2Z3eGZmYnpwcXJjZ3pya3dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2ODgwODUsImV4cCI6MjA5NjI2NDA4NX0.Q3izLxynvWOWuNMyoQmve9uZ_b8PKdk0DArSsIRqIUw",
   genkitApiUrl: "https://nerra-backend-nx0i.onrender.com",
